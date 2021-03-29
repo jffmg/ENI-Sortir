33587a98a3a9c9671ad31a7122dcc245707618d2
LeroyErwan <erwan.leroy2020@campus-eni.fr>
2021-03-29 21:03:29 +0200

    Message d'erreur si saisie d'un login et/ou mot de passe érronné

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
@@ -19,9 +20,12 @@ class MainController extends AbstractController
     /**
      * @Route("/login", name="user_login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('/participant/login.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('/participant/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
