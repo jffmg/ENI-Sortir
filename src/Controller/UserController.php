ebb79449a135e64bfa8967a0fca868a1d1604f3d
aline <marchal.aline@gmail.com>
2021-03-29 17:56:59 +0200

    formulaire de connexion (page twig)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,22 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/user")
+ * @Route("/participant")
  */
 class UserController extends AbstractController
 {
-    /**
-     * @Route("", name="user_login")
-     */
-    public function login(): Response
-    {
-        return $this->render('user/login.html.twig');
-    }
 
-    /**
-     * @Route("/logout", name="user_logout")
-     */
-    public function logout(): Response
-    {
-    }
 }
