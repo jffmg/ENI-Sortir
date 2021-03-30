c4c0e71dad091d8cb67c686f1bfc3d2881a79fb5
aline <marchal.aline@gmail.com>
2021-03-30 17:34:27 +0200

    list.twig suite de html

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Campus;
 use App\Entity\Event;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,12 +23,17 @@ class EventController extends AbstractController
         // Access denied if user not connected
         $this->denyAccessUnlessGranted("ROLE_USER");
 
+        // Get the campus from database
+        $campusRepo = $this->getDoctrine()->getRepository(Campus::class);
+        $campus = $campusRepo->findAll();
+
         // Get the events from database
         $eventRepo = $this->getDoctrine()->getRepository(Event::class);
         $events = $eventRepo->findAll();
 
         return $this->render("event/list.html.twig", [
-            "events" => $events
+            "events" => $events,
+            "campus" =>$campus
         ]);
     }
 
