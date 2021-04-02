5d230a6953c264b3ed5e097fcea3162ce9e9af24
aline <marchal.aline@gmail.com>
2021-04-02 09:12:13 +0200

    lien vers profil

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -129,15 +129,4 @@ class EventController extends AbstractController
             "event" => $event
         ]);
     }
-
-    /**
-     * @param int $variable
-     * @param string $appUserRole
-     *
-     * @return bool
-     */
-    public function inArray(int $variable, string $appUserRole): bool
-    {
-        return in_array($variable, json_decode($appUserRole));
-    }
 }
